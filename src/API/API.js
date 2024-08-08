cb1b91a0b08fbf830c4c59c07205f6f5788ca403
EkfrazoMohammed <mohammed@ekfrazo.in>
2024-08-08 17:29:53 +0530

    initial commit

--- src/API/API.js
+++ src/API/API.js
@@ -5,10 +5,6 @@ const   baseURL =  'https://hul.aivolved.in/api/';
 // const baseURL =  'http://vin.aivolved.in:8100/';
 // const baseURL =  'http://159.65.157.118:8005/';
 // const baseURL =  'http://hul.aivolved.in/api/';
-
-
-
-
 // const baseURL =  'http://localhost:8000/api/';
 
 const token = localStorage.getItem("token");
