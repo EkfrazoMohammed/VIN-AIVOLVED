c2abdd8689220ddcc1a644a3e3fed3b8f43516a7
EkfrazoMohammed <mohammed@ekfrazo.in>
2024-08-12 11:08:37 +0530

    adding encrpted form

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,56 +1,29 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import { persistStore, persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage/session'; // Use sessionStorage
-// import authReducer from './slices/authSlice';
-// import plantReducer from './slices/plantSlice';
-
-// // Persist Configurations for each slice
-// const authPersistConfig = {
-//   key: 'auth',
-//   storage,
-// };
-
-// const plantPersistConfig = {
-//   key: 'plant',
-//   storage,
-// };
-
-// // Combine reducers
-// const rootReducer = {
-//   auth: persistReducer(authPersistConfig, authReducer),
-//   plant: persistReducer(plantPersistConfig, plantReducer),
-// };
-
-// const store = configureStore({
-//   reducer: rootReducer,
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: false, // Necessary for Redux Persist
-//     }),
-// });
-
-// export const persistor = persistStore(store);
-// export default store;
-
-// store.js
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage/session'; // sessionStorage
+import storage from 'redux-persist/lib/storage'; // localStorage
+import { encryptTransform } from 'redux-persist-transform-encrypt';
 import authReducer from './slices/authSlice';
 import plantReducer from './slices/plantSlice';
-import encryptionTransform from './middleware/persistTransform';
+// Encryption Configuration
+const encryptor = encryptTransform({
+  secretKey: 'V!N_P0ND!', // Make sure to set this in your .env
+  onError: function(error) {
+    // Handle the error
+    console.error("Encryption error:", error);
+  },
+});
 
 // Persist Configurations for each slice
 const authPersistConfig = {
   key: 'auth',
   storage,
-  transforms: [encryptionTransform],
+  transforms: [encryptor],
 };
 
 const plantPersistConfig = {
   key: 'plant',
   storage,
-  transforms: [encryptionTransform],
+  transforms: [encryptor],
 };
 
 // Combine reducers
