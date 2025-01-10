95efc265a7c744d80813e18bba7abe31cc9e2e65
ASHIKEkfrazo <142868635+ASHIKEkfrazo@users.noreply.github.com>
2025-01-10 19:23:22 +0530

    Update API.js

--- src/API/API.js
+++ src/API/API.js
@@ -21,7 +21,7 @@ API.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem("token");
         if (token) {
-            config.headers.Authorization = Bearer ${JSON.parse(token)};
+            config.headers.Authorization = `Bearer ${JSON.parse(token)}`;
         }
         return config;
     },
